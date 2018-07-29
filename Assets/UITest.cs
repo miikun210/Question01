40341aca2db692196a943f8c0bbf430ba5745064
miikun <miikun210@gmail.com>
2018-07-29 17:39:50 +0900

    解決！
    504 自分：みーくん[sage] 投稿日：2018/07/29(日) 17:27:58.467 ID:mXQH8NIw0NIKU [7/8]
    解決なの！
    
    UI のレイヤーを「UI」に
    ワールドのレイヤーを「default」
    
    UI用カメラの描画対象を「UI」だけに（カリングマスクをUIだけに）
    ワールド用カメラの描画対象を「default」だけに（カリングマスクをdefaultだけに）
    
    UI用カメラのdepth値をあげて描画優先度を高くして
    描画時のクリアフラグを「Depth Only」に
    
    これで >>492　のおっしゃったとおりの状態になりました！
    
    わかりづらい質問から解決を引き出していただいてありがとうございました！

---
+++ Assets/UITest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITest : MonoBehaviour {
+
+	public RenderCameraScript renderCameraUI;
+	public RenderCameraScript renderCameraWorld;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Click_UI()
+	{
+		renderCameraWorld.enabled = false;
+		renderCameraUI.enabled = true;
+	}
+
+	public void Click_World()
+	{
+		renderCameraUI.enabled = false;
+		renderCameraWorld.enabled = true;
+	}
+
+	public void Click_Resume()
+	{
+		renderCameraUI.enabled = false;
+		renderCameraWorld.enabled = false;
+	}
+	
+}
